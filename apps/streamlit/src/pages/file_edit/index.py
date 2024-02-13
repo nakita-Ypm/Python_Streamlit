ff18df3f05b65512619a35d3da0b44ed81532ab7
Naoki Akita <nakita@ypm.llc>
2024-02-13 22:28:43 +0900

    コミット

--- apps/streamlit/src/pages/file_edit/index.py
+++ apps/streamlit/src/pages/file_edit/index.py
@@ -12,10 +12,18 @@ def apply(req):
     uploaded_file = up.upload(req["upload_title"], req["type"], req["decode"])
 
     if uploaded_file is not None:
-        push_button(req, uploaded_file)
+        text = push_button(req, uploaded_file)
+        st.text(text)
 
+        if text is not None:
+            download_txt_button(text)
+
+    
 
 def push_button(req, uploaded_file):
     if st.button(req["button"]):
         res = post.post_file(req["api_endpoint"], req["flie_type"], uploaded_file)
-        st.text(res.text)
+        return res.text
+
+def download_txt_button(text):
+    st.download_button('Download',text)
